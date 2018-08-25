a7524547e140ec17c97fd1d68234a623d202ae47
Taimir Aguacil <taimiraguacil15@hotmail.com>
2018-08-25 02:38:53 +0200

    Finalization of basic implementation. Known issues : restricted DCT N (must be implemented on incoming data) Normalization and lat/lon resolution handling is bad since numbers are either too small or too big. (IOS supports 16 bit float only)

--- gpsData/CompressSensing.swift
+++ gpsData/CompressSensing.swift
@@ -50,7 +50,10 @@ class CompressSensing {
     var lon_est : [Float]
     var latValArray : Array<Float>
     var lonValArray : Array<Float>
-    var realVector : ValueArray<Float>
+    var meanLat = Float(0)
+    var meanLon = Float(0)
+    var stdLat  = Float(0)
+    var stdLon  = Float(0)
     
     lazy var dctSetupForward: vDSP_DFT_Setup = {
         guard let setup = vDSP_DCT_CreateSetup(
@@ -74,7 +77,7 @@ class CompressSensing {
     }()
     
     let numberOfSamples : Int
-    let ratio = 1.0
+    let ratio = 1.0 // with .x because double
     let l1_penalty = Float(0.01) // learning rate
     let tolerance = Float(0.0001)
     let lassModel = LassoRegression()
@@ -86,6 +89,8 @@ class CompressSensing {
         self.lonArray_org = []
         self.lat_est = []
         self.lon_est = []
+        self.latValArray = []
+        self.lonValArray = []
         
         // Extract the coordinates from the location vector if it exists
         guard !locationVector.isEmpty else {
@@ -95,9 +100,8 @@ class CompressSensing {
         
         self.totalNumberOfSamples = locationVector.count
         self.numberOfSamples = Int(floor(Double(totalNumberOfSamples)*ratio))
-        self.latValArray = Array<Float> (repeating:0, count: Int(numberOfSamples))
-        self.lonValArray = Array<Float> (repeating:0, count: Int(numberOfSamples))
-        self.realVector = ValueArray<Float> (capacity: totalNumberOfSamples)
+        //self.latValArray = Array<Float> (repeating:0, count: Int(numberOfSamples))
+        //self.lonValArray = Array<Float> (repeating:0, count: Int(numberOfSamples))
         
         for item in locationVector {
             self.latArray_org.append(Float(item.coordinate.latitude))
@@ -126,6 +130,12 @@ class CompressSensing {
             latValArray.append(latArray_org[item])
             lonValArray.append(lonArray_org[item])
         }
+        meanLat = mean(latValArray)
+        meanLon = mean(lonValArray)
+        stdLat  = std(latValArray)
+        stdLon  = std(lonValArray)
+        latValArray = Array((latValArray-meanLat)/(stdLat))
+        lonValArray = Array((lonValArray-meanLon)/(stdLon))
         return downSampledIndices
     }
     
@@ -133,7 +143,7 @@ class CompressSensing {
     func forwardDCT<M: LinearType>(_ input: M) -> [Float] where M.Element == Float {
         os_log("Forward DCT", log: OSLog.default, type: .debug)
         var results = Array<Float>(repeating:0, count: Int(totalNumberOfSamples))
-        realVector.assign(input) // append here is wrong 
+        let realVector = ValueArray<Float>(input)
         vDSP_DCT_Execute(dctSetupForward,
                          realVector.pointer,
                          &results)
@@ -145,7 +155,7 @@ class CompressSensing {
     func inverseDCT<M: LinearType>(_ input: M) -> [Float] where M.Element == Float {
         os_log("Inverse DCT", log: OSLog.default, type: .debug)
         var results = Array<Float>(repeating:0, count: Int(totalNumberOfSamples))
-        realVector.assignFrom(input)
+        let realVector = ValueArray<Float>(input)
         vDSP_DCT_Execute(dctSetupInverse,
                          realVector.pointer,
                          &results)
@@ -156,23 +166,43 @@ class CompressSensing {
     func eyeDCT(downSampledIndices: [Int]) -> [Array<Float>] {
         os_log("Identiy DCT function", log: OSLog.default, type: .debug)
         var pulseVector = Array<Float>(repeating: 0.0, count: totalNumberOfSamples)
-        var dctMat = Array<Array<Float>>() // TODO not efficient
+        let dctVec = ValueArray<Float>(capacity: numberOfSamples*totalNumberOfSamples)
+        var dctMat = Matrix<Float>(rows: numberOfSamples, columns: totalNumberOfSamples)
+        var dctArrayMat = Array<Array<Float>>() // TODO not efficient
         for item in downSampledIndices {
             pulseVector[item] = 1.0
             let dctVector = forwardDCT(pulseVector)
             pulseVector[item] = 0.0
-            dctMat.append(dctVector)
+            dctVec.append(contentsOf: dctVector)
         }
-        return dctMat
+        dctMat = dctVec.toMatrix(rows: numberOfSamples, columns: totalNumberOfSamples)
+        print(dctVec.description)
+        print(dctMat.description)
+        
+        var temp = Array<Float>()
+        for col in 0..<dctMat.columns
+        {
+            for row in 0..<dctMat.rows
+            {
+                temp.append(dctMat[row, col])
+            }
+            dctArrayMat.append(temp)
+            temp.removeAll(keepingCapacity: true)
+        }
+        return dctArrayMat
     }
     
     /* Performs Lasso regression */
     func lassoReg (dctMat : [Array<Float>]){
         os_log("Lasso regression function", log: OSLog.default, type: .debug)
         // Set Initial Weights
-        let initial_weights_lat = Matrix<Float>(rows: totalNumberOfSamples, columns: 1, repeatedValue: 0)
-        let initial_weights_lon = Matrix<Float>(rows: totalNumberOfSamples, columns: 1, repeatedValue: 0)
+        let initial_weights_lat = Matrix<Float>(rows: totalNumberOfSamples+1, columns: 1, repeatedValue: 0)
+        let initial_weights_lon = Matrix<Float>(rows: totalNumberOfSamples+1, columns: 1, repeatedValue: 0)
         weights_lat = try! lassModel.train(dctMat, output: latValArray, initialWeights: initial_weights_lat, l1Penalty: l1_penalty, tolerance: tolerance)
+        print(dctMat.description)
+        print(latValArray.description)
+        print(weights_lat.description)
+        print(weights_lat.column(1).description)
         weights_lon = try! lassModel.train(dctMat, output: latValArray, initialWeights: initial_weights_lon, l1Penalty: l1_penalty, tolerance: tolerance)
     }
     
@@ -181,6 +211,8 @@ class CompressSensing {
         os_log("IDCT of weights", log: OSLog.default, type: .debug)
             lat_est = inverseDCT(weights_lat.column(1))
             lon_est = inverseDCT(weights_lon.column(1))
+        lat_est = Array((lat_est*stdLat)+meanLat)
+        lon_est = Array((lon_est*stdLon)+meanLon)
     }
     
     /* Perform entire computation */
